b4edcbfd4b56bee9dd2541e9ff48e21101998665
Patrick McDonald <764290+WhatsThatItsPat@users.noreply.github.com>
2022-05-12 14:18:59 -0500

    Play with dynamically creating div in directive

--- src/app/bg-scroll.directive.ts
+++ src/app/bg-scroll.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
 import { DomController, ScrollCustomEvent } from '@ionic/angular';
 
 @Directive({
@@ -10,8 +10,22 @@ export class BgScrollDirective implements OnInit {
 
   constructor(
     private domCtrl: DomController,
-    private renderer: Renderer2
-  ) {}
+    private renderer: Renderer2,
+    private el: ElementRef,
+  ) {
+    console.log(`nativeElement`, this.el.nativeElement);
+    console.log(`parentNode`, this.el.nativeElement.parentNode);
+    console.log(`attributes`, this.el.nativeElement.attributes);
+    console.log(`getContentAttr in constructor`, this.getContentAttr());
+
+    const newDiv = this.renderer.createElement('div');
+    this.renderer.setAttribute(newDiv, 'slot', 'fixed');
+    this.renderer.setAttribute(newDiv, this.getContentAttr(), '');
+    this.renderer.addClass(newDiv, 'howdy');
+    const newText = this.renderer.createText('howdy');
+    this.renderer.appendChild(newDiv, newText);
+    this.renderer.appendChild(this.el.nativeElement, newDiv);
+  }
 
   @HostListener('ionScroll', ['$event']) onContentScroll(event: ScrollCustomEvent) {
     const scrollTop = event.detail.scrollTop;
@@ -73,6 +87,38 @@ export class BgScrollDirective implements OnInit {
       */
      this.renderer.setStyle(this.bg, 'transitionDuration', '70ms');
      this.renderer.setStyle(this.bg, 'transitionTimingFunction', 'ease-out');
+  }
+
+  /**
+   * Angular View Encapsulation creates unique _nghost and _ngcontent ids
+   * for a component and its children respectively. This function gets the
+   * _ngcontent of ion-content (which is a child of the page component)
+   * so we can apply the same attribute to the background div we are
+   * dynamically creating. This lets us use CSS on it like normal by putting
+   * it in the SCSS file linked to in the @Component decorator.
+   *
+   * This is a bit hacky. It reduces the boilerplate a bit (otherwise we'd need
+   * to create template reference variables and pass them in to the directive),
+   * but Angular could change under our feet:
+   *
+   *    https://angular.io/guide/view-encapsulation#inspecting-generated-css
+   *    > The exact values of these attributes are a private implementation
+   *        detail of Angular. They are automatically generated and you
+   *        should never refer to them in application code.
+   *
+   * We don't really care about the "exact value" as the docs warn against (the
+   * exact value has a random suffix lik `-juo-c141` that is created each time
+   * the app runs, so we could never know or use that specific value), so I think
+   * we're OK. Without this, we'd have to add an empty div to each ion-content,
+   * give it a Template Reference Variable, and pass that variable to the directive.
+   *
+   *  TODO add example
+   */
+   getContentAttr(): string {
+    return [
+      // Turn NamedNodeMap into an array so we can use `find()`
+      ...this.el.nativeElement.attributes
+    ].find(attribute => attribute.name.startsWith('_ngcontent')).name;
   }
 
 }
