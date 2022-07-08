ab96645b5a296c665bc4e887462083fbdb2c5418
Patrick McDonald <764290+WhatsThatItsPat@users.noreply.github.com>
2022-07-08 09:46:00 -0400

    Add simple test

--- src/app/bg-scroll.directive.spec.ts
+++ src/app/bg-scroll.directive.spec.ts
@@ -1,8 +1,18 @@
+import { ElementRef, Renderer2 } from '@angular/core';
+import { inject } from '@angular/core/testing';
+import { DomController } from '@ionic/angular';
 import { BgScrollDirective } from './bg-scroll.directive';
 
 describe('BgScrollDirective', () => {
   it('should create an instance', () => {
-    const directive = new BgScrollDirective();
-    expect(directive).toBeTruthy();
+
+    inject(
+      [DomController, Renderer2, ElementRef],
+      (domController: DomController, renderer: Renderer2, el: ElementRef) => {
+        const directive = new BgScrollDirective(domController, renderer, el);
+        expect(directive).toBeTruthy();
+      }
+    );
+
   });
 });
